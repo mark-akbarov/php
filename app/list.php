5c405750c5c476b38f9aba7e7b945dc2e1c50c66
mark-akbarov <markakbarov@gmail.com>
2023-12-20 16:32:00 +0500

    add retrieve blog

--- app/list.php
+++ app/list.php
@@ -1,4 +1,5 @@
 <?php 
+
     include '../inc/header.php'; 
 
     # Establish Connection
@@ -9,27 +10,15 @@
         die("Connection Failed");
     }
 
-    if (isset($_GET['id'])) {
-        # Retrieve the 'id' value from the URL
-        $blogId = $_GET['id'];
-    
-    } else {
-        # Handle the case where 'id' parameter is not set
-        echo "Blog ID is missing in the URL.";
-    }
-
     # Raw SQL query
-    $sql = "SELECT id, title, body FROM blog WHERE id=$blogId";
-    
+    $sql = "SELECT id, title, body FROM blog";
     $query = pg_query($conn, $sql);
     
     if (!$query) {
         die("Query failed to execute: " . pg_last_error($conn));
     }
-    
-    $blog = pg_fetch_assoc($query);
 
-    pg_free_result($query);
+    $blogs = pg_fetch_all($query);
 
     $db->closeConnection();
 
@@ -37,26 +26,22 @@
 
 <div class="container-md mt-5 w-50">
     <h3>Blogs</h3>
-    <?php if (empty($blog)): ?>
-        <p>Blog Not Found.</p>
-    <?php else: ?>
+    <?php foreach ($blogs as $blog): ?>
         <div class="card text-center mt-5 mb-5">
             <div class="card-header">
-                <?php echo $blog["id"]; ?>
+                <p>Blog No: <?php echo $blog['id']; ?>
             </div>
             <div class="card-body">
-                <h5 class="card-title">
-                    <?php echo $blog["title"]; ?>
-                </h5>
-                <p class="card-text">
-                    <p><?php echo $blog["body"]; ?></p>
+                <h5 class="card-title"><p><?php echo $blog['title'] ?></h5>
+                <p class="card-text"><p><?php echo $blog['body'] ?></p>
+                <?php ?>
+                <a href="retrieve.php?id=<?php echo $blog['id']; ?>" class="btn btn-primary">Read More</a>
             </div>
             <div class="card-footer text-body-secondary">
                 2 days ago
             </div>
         </div>
-    <?php endif; ?>
-
+    <?php endforeach; ?>
 </div>
 </div>   
 </body>
